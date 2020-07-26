db630ae582c357121f7877847eaef28418f6a5e3
humnaS <humna.shafqatiqbal@gmail.com>
2020-07-27 00:23:50 +0500

    first commit

---
+++ blue/api/Functions.py
@@ -0,0 +1,184 @@
+import numpy as np
+import pandas as pd
+from flask import Blueprint, jsonify, request
+from flask_restful import Api, Resource
+from sklearn import preprocessing
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import sklearn
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+
+
+df=pd.DataFrame()
+data=pd.DataFrame()
+my_df=pd.DataFrame()
+dic=dict()
+name_col=['Message Size', 'Incoming Header lines Count', 'Sender-IP','Number of complaints (BCL)', 'Inbox']
+
+def get_file(my_file):
+    global df
+   
+    try:
+        print("hello from get file")
+        file_rec = my_file
+      
+       
+        df = pd.read_csv(file_rec.stream,delimiter=";")
+        
+        hold_data()
+        
+        retJson = {"status":200,"msg":"ok"}
+        return True
+    except Exception as e:
+ 
+        try:
+            file_rec = request.files['file']
+
+            df = pd.read_excel(file_rec.stream)
+      
+          
+            retJson = {"status":200,"msg":"ok"}
+            return True
+        except Exception as e:
+            try:
+                
+                file_rec = my_file
+            
+            
+                df = pd.read_csv(file_rec.stream)
+                
+                hold_data()
+                print(df.head())
+                retJson = {"status":200,"msg":"ok"}
+                return True
+                            
+                
+            except Exception as e:
+                retJson = {"status":301,"msg":"This file format is not supported"}
+            return False
+
+
+def list_contains(List1, List2):
+    count=0
+    # Iterate in the 1st list 
+    for m in List1: 
+  
+        # Iterate in the 2nd list 
+        for n in List2: 
+    
+            # if there is a match
+            if m == n: 
+                count=count+1
+                  
+                  
+    return count
+
+def hold_data():
+    global data
+    #print("hello from hold")
+    data=df.copy()
+    
+    #print(data.head())
+    return data
+
+def chk_col(df):
+    global my_df
+    my_df=df.copy()
+    cols=my_df.columns
+    cols=list(cols)
+    
+    check =  any(item in name_col for item in cols)
+    print(check)
+    if check ==True:
+        count=list_contains(name_col,cols)
+        print(count)
+        if count==5:
+            print("hello from inside")
+            my_df=my_df[['Message Size', 'Incoming Header lines Count', 'Sender-IP','Number of complaints (BCL)', 'Inbox']]
+            my_df.dropna(inplace=True)
+            my_df=my_df.drop("Sender-IP",axis=1)
+            my_df["cat"] = my_df["Inbox"].replace({"Yes": 1, "NO": 0,"YES":1,"No":0})
+            X=my_df.drop(["Inbox","cat"],axis=1)
+            y=my_df["cat"]
+             
+            print(type(y))
+            return True,X,y
+    else:
+        
+        return False,None,None
+        
+def chcking():
+    global a
+    try:
+        chk,X,y=chk_col(data)
+        print(chk)
+        if chk== True:
+            print(X.head())
+            print(y)
+            a=Train_model(X,y)
+            find_model()
+            return{"status":"200","msg":"Ok"}
+        else:
+            return{"error":"your data shape is not correct"}
+            
+    except Exception as e:
+        print("masla")
+        dic2={"error":"your data shape is not correct","msg":str(e)}
+        return dic2
+
+
+def Train_model(X,y):
+    global dic
+    print("Hllo from training")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+    model_decision = tree.DecisionTreeClassifier()
+    model_decision.fit(X_train, y_train)
+    y_pred_d= model_decision.predict(X_test)
+    accuracy=accuracy_score(y_test, y_pred_d)
+
+    ####################################
+    
+    model_svm = LinearSVC()
+    model_svm.fit(X_train, y_train)
+    y_pred_svm = model_svm.predict(X_test)
+
+    accuracy2=accuracy_score(y_test, y_pred_svm)
+
+    ##########################################
+    model_rdm = RandomForestClassifier(n_estimators = 10, criterion = 'entropy') 
+							
+    model_rdm.fit(X_train, y_train) 
+    y_pred_rdm = model_rdm.predict(X_test)
+
+    accuracy3=accuracy_score(y_test, y_pred_rdm)
+    dic={accuracy:model_decision,accuracy2:model_svm,accuracy3:model_rdm}
+    
+    my_list=[accuracy,accuracy2,accuracy3]
+    print(my_list)
+    my_list.sort(reverse=True)
+    my_model=dic[my_list[0]]
+    print(my_model)
+    return my_list[0]
+
+
+def find_model():
+    model=dic[a]
+    return model
+def prediction(msg,msg1,msg2):
+    try:
+        model=find_model()
+        print(model)
+        pred=model.predict([[msg,msg1,msg2]])
+        if pred==1:
+            return{"Result":"Yes"}
+        elif pred==0:
+            return{"Result":"No"}
+    except Exception as e:
+        dic2={"error":"your data shape is not correct","msg":str(e)}
+        return dic2
+    
+    
+   
---
+++ blue/api/routes.py
@@ -0,0 +1,64 @@
+from flask import Blueprint,jsonify, request
+from flask_restful import Api,Resource
+import pandas as pd
+from werkzeug.utils import secure_filename
+
+
+import sys 
+import os
+import json
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+dirname_list = dirname.split("/")[:-1]
+dirname = "/".join(dirname_list)
+print(dirname)
+path = dirname + "/api"
+print(path)
+sys.path.append(path)
+
+
+mod = Blueprint('api',__name__)
+api = Api(mod)
+df = None
+
+from Functions import get_file,chcking,prediction
+
+
+class hello(Resource):
+    def get(self):
+        retJson = {"status":200,"msg":"ok"}
+        return jsonify(retJson)
+
+
+class data(Resource):
+    def post(self):
+        file_rec = request.files['file']
+        print("hello")
+        chk=get_file(file_rec)
+        if chk==True:
+            ret= chcking()
+            return jsonify(ret)
+        else:
+            retJson = {"status":301,"msg":"This file format is not supported"}
+            return jsonify(retJson)
+
+class Prediction(Resource):
+    def post(self):
+        postedData=request.get_json()
+        print(postedData)
+        msg=postedData['Message_Size']
+        
+        msg1=postedData['Incoming_Header_lines_Count']
+        msg2=postedData['Number_of_complaints_(BCL)']
+        msg3=postedData['Sender_IP']
+       
+        ret=prediction(msg,msg1,msg2)
+        #ret=str(ret[0])
+        return jsonify(ret)
+  
+
+
+api.add_resource(hello, "/hello")
+api.add_resource(data, "/data")
+api.add_resource(Prediction, "/pred")
